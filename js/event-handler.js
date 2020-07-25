03d03eaac4af9d6f3e92665f428c52f8144e3f5d
carrasc <luiscbilbao@gmail.com>
2020-07-24 19:56:48 -0500

    Clock pause, resume, start, gui, autoScale, etc

--- js/event-handler.js
+++ js/event-handler.js
@@ -4,8 +4,96 @@ function resizeWindow(event)
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     engine.setSize(canvas.width, canvas.height);
-    camera.aspect = canvas.width / canvas.height;
-    camera.updateProjectionMatrix();
+    if(!multiview)
+    {
+        // OBSERVER  CAMERA
+        cameraPerspective.aspect = canvas.width / canvas.height;
+        cameraPerspective.updateProjectionMatrix();
+    }
+    else
+    {
+        // OBSERVER CAMERA
+        cameraOrtho.aspect = canvas.width / 2. / canvas.height;
+        cameraOrtho.updateProjectionMatrix();
+
+        // ACTIVE CAMERA
+        cameraFirstPerson.aspect = canvas.width / 2. / canvas.height;
+        cameraFirstPerson.updateProjectionMatrix();
+    }
+}
+
+function keyDownEventListener(event)
+{ 
+	if(event.keyCode == 32)	// Space bar
+	{
+		multiview = !multiview;
+	}
+}
+
+function sliderBaseListener()
+{
+    ramp.scale.x = guiControls.base;
+
+    box = new THREE.Box3().setFromObject( ramp );
+    var test = new THREE.Vector3();
+    box.getSize(test);
+
+    // Update the cube position
+    cube.position.set(box.min.x, box.max.y + 0.7, 0);
+    cube.rotation.z = -(angle);
+
+    // With the height and the base, we get the angle and the distance
+    angle = getAngle(test.y, test.x)
+
+    // Update camera fpv
+    cameraFirstPerson.position.set(box.min.x, box.max.y + 1, 0.); 
+}
+
+function sliderHeightListener()
+{
+    ramp.scale.y = guiControls.height;
+
+    box = new THREE.Box3().setFromObject( ramp );
+    var test = new THREE.Vector3();
+    box.getSize(test);
+
+    // Move the floor to match the ramp
+    floor.position.y = -test.y / 2 - 1.;
+    floorHelper.position.y = -test.y / 2;
+
+    // Update the cube position
+    cube.position.set(box.min.x, box.max.y + 0.7, 0);
+    cube.rotation.z = -(angle);
+
+    // With the height and the base, we get the angle and the distance
+    angle = getAngle(test.y, test.x)
+
+    // Update camera fpv
+    cameraFirstPerson.position.set(box.min.x + 0.1, box.max.y, 0.);  
+}
+
+function startSimulation()
+{
+    start = true;
+    pause = false;
+
+    // Update the cube position
+    cube.position.set(box.min.x, box.max.y + 0.7, 0);
+    cube.rotation.z = -(angle);
+
+    // Update camera fpv
+    cameraFirstPerson.position.set(box.min.x + 0.1, box.max.y, 0.);  
+
+    // Compute the simulation given the mass, roz coeficient and angle 
+    accel = getVariables(2, 0.3, angle);
+
+    pausedTime = 0;
+    clock.start();
+}
+
+function pauseSimulation()
+{
+    pause = !pause;
 }
 
 
--- js/main.js
+++ js/main.js
@@ -2,7 +2,20 @@
 var canvas;
 var engine;
 var scene;
-var camera;
+
+// Camera variables
+var cameraPerspective;
+var cameraOrtho;
+var cameraFirstPerson;
+var multiview = false;
+var controlCameraActive;
+var controlPerspective;
+
+var floor;
+var floorHelper;
+
+var ramp;
+
 var light;
 var cube;
 var axesHelperLocal;
@@ -10,9 +23,15 @@ var gravity = 9.81;
 var accel;
 var speed;
 var clock = new THREE.Clock();
+var pausedTime = 0;
 var delta;
 var angle = 0;
 var stats = new Stats();
+var initialSpeed = 0;
+
+var guiControls;
+var start = false;
+var pause = false;
 
 
 // The total time it will take the object to get to the bottom
@@ -115,33 +134,86 @@ class Axes extends THREE.Mesh
 
 function update()
 {
-    
-    
-}
-
-function renderLoop() 
-{
     stats.begin();
     delta = clock.getDelta();
-    engine.render(scene, camera);
+    
+    // Paused time is 0 at first, but if the user pauses it adds that delta (which is the time the clock was paused) to this variable
+    var elapsedTime = clock.elapsedTime - pausedTime;
+
     if (cube.position.x <= box.max.x && accel > 0)
     {
         //console.log("speed: ", speed, " at time: ", clock.elapsedTime)
-        speed = (0 + accel)*clock.elapsedTime;
+        speed = (initialSpeed + accel)*elapsedTime;
 
-        cube.position.x += (speed*delta)*Math.cos(angle);
-        cube.position.y -= (speed*delta)*Math.sin(angle);
+        // If the delta is mora than 0.1 it means the user probabilly paused, in which case we add that to the pausedTime variable
+        if (delta < .1)
+        {
+            cube.position.x += (speed*delta)*Math.cos(angle);
+            cube.position.y -= (speed*delta)*Math.sin(angle);
+
+            cameraFirstPerson.position.x += (speed*delta)*Math.cos(angle);
+            cameraFirstPerson.position.y -= (speed*delta)*Math.sin(angle);
+        }
+        else 
+        {
+            pausedTime += delta;
+        }
+        
 
         axesHelperLocal.update(clock.getDelta());
-        //cube.rotation.y = cube.rotation.y + 0.01;
     }
+
     else 
     {
         clock.stop();
-        console.log(clock.elapsedTime)
+        //console.log(clock.elapsedTime)
     }
-    update();
+
     stats.end();
+}
+
+
+function renderLoop() 
+{
+    engine.setViewport(0, 0, window.innerWidth, window.innerHeight);
+    engine.clear();
+
+    if(!multiview)
+    {
+        // Camera Observer
+        cameraPerspective.aspect = canvas.width / canvas.height;
+        cameraPerspective.updateProjectionMatrix();
+        engine.setScissor(0, 0, window.innerWidth, window.innerHeight);
+        engine.render(scene, cameraPerspective);
+    }
+    else
+    {
+        var w = window.innerWidth;
+        var h = window.innerHeight;
+        // OBSERVER CAMERA
+        cameraOrtho.aspect = w/2. / h;
+        cameraOrtho.updateProjectionMatrix();
+       
+        engine.setViewport(w/2., 0, w/2., h);
+        engine.setScissor(w/2., 0, w/2., h);
+
+        engine.render(scene, cameraOrtho);
+
+        // ACTIVE CAMERA
+        cameraFirstPerson.aspect = w/2. / h;
+        cameraFirstPerson.updateProjectionMatrix();
+        engine.setViewport(0, 0, w/2., h);
+        engine.setScissor(0, 0, w/2., h);
+        engine.render(scene, cameraFirstPerson);
+    }
+
+    if (start && !pause)
+    {
+        update();
+    }
+
+    //console.log(clockPause.elapsedTime)
+    //console.log(clockPause.oldTime);
     requestAnimationFrame(renderLoop);
 }
 
@@ -157,41 +229,48 @@ function main()
     // RENDERER ENGINE
     engine = new THREE.WebGLRenderer({canvas: canvas});
     engine.setSize(window.innerWidth, window.innerHeight);
-    engine.setClearColor(new THREE.Color(0.9411, 0.9411, 0.9411), 1.);   
+    engine.setClearColor(new THREE.Color(0.9411, 0.9411, 0.9411), 1.);
+    engine.setScissorTest(true);
+    engine.autoclear = false;     
 
     // FLOOR
     var planeGeometry = new THREE.PlaneBufferGeometry( 250, 250 );
     planeGeometry.rotateX( - Math.PI / 2 );
     var planeMaterial = new THREE.ShadowMaterial( { opacity: 0.2 } );
 
-    var floor = new THREE.Mesh( planeGeometry, planeMaterial );
+    floor = new THREE.Mesh( planeGeometry, planeMaterial );
     floor.position.y = -1;
     floor.receiveShadow = true;
 
     // FLOOR HELPER
-    var helper = new THREE.GridHelper( 250, 250 );
-    helper.material.opacity = 0.25;
-    helper.material.transparent = true;
+    floorHelper = new THREE.GridHelper( 250, 250 );
+    floorHelper.material.opacity = 0.25;
+    floorHelper.material.transparent = true;
 
     // MODELS
     // THE RAMP
-    var mesh = new Triangle3D(); 
-    mesh.scale.set(100, 50, 1.);
+    ramp = new Triangle3D(); 
+    ramp.scale.set(50, 50, 1.);
 
-    box = new THREE.Box3().setFromObject( mesh );
+    // Create a Bounding Box for the ramp to know its height and width
+    box = new THREE.Box3().setFromObject( ramp );
     triangle3dsize = new THREE.Vector3();
     box.getSize(triangle3dsize);
-    //mesh.position.y = size.y / 2.;
+
+    // Move the floor to match the ramp
+    floor.position.y = -triangle3dsize.y / 2 - 1.;
+    floorHelper.position.y = -triangle3dsize.y / 2;
+
+    // With the height and the base, we get the angle and the distance
     angle = getAngle(triangle3dsize.y, triangle3dsize.x)
+    // Compute the simulation given the mass, roz coeficient and angle 
     accel = getVariables(2, 0.3, angle);
-    console.log(box)
 
 
-    // CUBE
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({wireframe: true, color: "black"});
-    cube = new THREE.Mesh(geometry, material);
+    // THE CUBE
+    cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({wireframe: true, color: "black"}));
     cube.position.set(box.min.x, box.max.y + 0.7, 0);
+    // Rotate the angle (negative) so it matches the ramp and it looks like its sliding down
     cube.rotation.z = -(angle);
     
     // THREE js Axes Helper (for global scene)
@@ -204,27 +283,64 @@ function main()
     scene = new THREE.Scene();  
     scene.add(floor);
     scene.add(cube);    // CUBO   
-    scene.add(mesh);
+    scene.add(ramp);
     scene.add(axesHelperLocal);
     scene.add(axesHelper);
-    scene.add( helper );
+    scene.add(floorHelper);
 
     // CAMERA
     //camera = new THREE.OrthographicCamera(  );
-    camera = new THREE.PerspectiveCamera(60., canvas.width / canvas.height, 0.01, 10000.);  // CAMERA
-    camera.position.set(0., 0, 80.);         
+    // OBSERVER PERSPECTIVE
+    cameraPerspective = new THREE.PerspectiveCamera(60., canvas.width / canvas.height, 0.1, 10000.);  // CAMERA
+    cameraPerspective.position.set(0, box.max.y, 100)
+    cameraPerspective.lookAt(scene.position); 
+    cameraPerspective.up.set(0., 1., 0.);  
+
+    // OBSERVER ORTHO
+    cameraOrtho = new THREE.OrthographicCamera(- 250, 250, 250, -250, 1, 1000 );  // CAMERA
+    cameraOrtho.position.set(0, 0, 100)
+    cameraOrtho.lookAt(scene.position); 
+    cameraOrtho.zoom = 3;  
+    cameraOrtho.up.set(0., 1., 0.);  
+
+    // CAMERA FIRST PERSON
+    cameraFirstPerson = new THREE.PerspectiveCamera(60., 0.5*canvas.width / canvas.height, 0.1, 1000.);  // CAMERA
+    cameraFirstPerson.position.set(box.min.x + 0.1, box.max.y, 0.);  
+    cameraFirstPerson.rotation.y = -(Math.PI / 180)*90;  
+    
+    var cameraHelper = new THREE.CameraHelper(cameraFirstPerson);
  
-    var controls = new THREE.OrbitControls(camera, canvas);   
-    scene.add(camera);  
+    controlPerspective = new THREE.OrbitControls(cameraPerspective, canvas);   
+    scene.add(cameraPerspective);  
+    scene.add(cameraFirstPerson);
+    scene.add(cameraOrtho);
+    //scene.add(cameraHelper);
 
     // LIGHTS
     light = new THREE.AmbientLight();  
     scene.add(light); 
+
+    // GUI
+    guiControls = { deltaThetaX: 0., 
+        deltaThetaY: 0.,
+        base: 0, 
+        height: 0, 
+        buttonStart: startSimulation, 
+        buttonPause: pauseSimulation
+      };
+    var datGui = new dat.GUI();
+    var sliderBase = datGui.add(guiControls, 'base', 2., 150., 0.01);
+    var sliderHeight = datGui.add(guiControls, 'height', 2., 150., 0.01);
+    var buttonStart = datGui.add(guiControls, 'buttonStart').name('Start');
+    var buttonPause = datGui.add(guiControls, 'buttonPause').name('Pause/Play');
+    datGui.close();
                                
     // EVENT-HANDLERS
     window.addEventListener('resize', resizeWindow, false);
+    document.addEventListener("keydown", keyDownEventListener, false);
+    sliderBase.onChange(sliderBaseListener);
+    sliderHeight.onChange(sliderHeightListener);
 
-    clock.start();
     // ACTION
     requestAnimationFrame(renderLoop);           
 }
